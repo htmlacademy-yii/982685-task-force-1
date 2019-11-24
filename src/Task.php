db89d19cc14b9e5c0f73aa0c20b6a6bd6dd85cbb
Somov Eugene <somov.eugene.2012@gmail.com>
2019-11-24 16:57:52 +0300

    задание «Камень судьбы».

---
+++ src/Task.php
@@ -0,0 +1,94 @@
+<?php
+namespace taskforce;
+
+class Task
+{
+    /**
+     * список всех доступных статусов задач
+     */
+    const STATUS_NEW = 'new';               // Новое - задание опубликовано, исполнитель ещё не найден
+    const STATUS_CANCELED = 'canceled';     // Отменено - заказчик отменил задание
+    const STATUS_PROGRESS = 'progress';     // В работе - заказчик выбрал исполнителя для задания
+    const STATUS_COMPLETED = 'completed';   // Выполнено - заказчик отметил задание как выполненное
+    const STATUS_FAILED = 'failed';         // Провалено - исполнитель отказался от выполнения задания
+
+    /**
+     * список всех возможных ролей пользователей
+     */
+    const ROLE_CUSTOMER = 'customer';       // Заказчик
+    const ROLE_EXECUTOR = 'executor';       // Исполнитель
+
+    /**
+     * список всех доступных действий над задачами
+     */
+    // Заказчик
+    const ACTION_CANCEL = 'cancel';         // Отменить
+    const ACTION_APPOINT = 'appoint';       // Назначить
+    const ACTION_COMPLETE = 'complete';     // Завершить
+    // Исполнитель
+    const ACTION_RESPOND = 'respond';       // Откликнуться
+    const ACTION_REFUSE = 'refuse';         // Отказаться
+
+    /**
+     * СВОЙСТВА класса
+     */
+    private $id_customer = null;            // id заказчика
+    private $id_executor = null;            // id исполнителя
+    private $completion_date = null;        // срок завершения задачи
+    private $current_status = null;         // активный статус
+
+    /**
+     * КОНСТРУКТОР класса
+     * @param int $id_customer ID заказчика
+     * @param int $id_executor ID исполнителя
+     * @param string $completion_date Срок завершения задачи
+     */
+    public function __construct(int $id_customer, ?int $id_executor = null, ?string $completion_date = null)
+    {
+        $this->id_customer = $id_customer;
+        $this->id_executor = $id_executor;
+        $this->completion_date = $completion_date;
+        $this->current_status = self::STATUS_NEW;
+    }
+
+    // МЕТОДЫ класса
+
+    /**
+     * Возвращает список действий
+     * @return array
+     */
+    public function get_actions(): array
+    {
+        return [self::ACTION_CANCEL, self::ACTION_APPOINT, self::ACTION_COMPLETE, self::ACTION_RESPOND, self::ACTION_REFUSE];
+    }
+
+    /** Возвращает список статусов
+     * @return array
+     */
+    public function get_statuses(): array
+    {
+        return [self::STATUS_NEW, self::STATUS_CANCEL, self::STATUS_PROGRESS, self::STATUS_COMPLETED, self::STATUS_FAILED];
+    }
+
+    /** Возвращает статус, в который перейдет задача для указанного действия
+     * @param string $action Действие
+     * @return string|null Статус задачи
+     */
+    public function get_next_status(string $action): string
+    {
+        // доступные действия и изменение состояния задачи
+        $links = [
+            self::ACTION_CANCEL => STATUS_CANCELED,
+            self::ACTION_APPOINT => STATUS_PROGRESS,
+            self::ACTION_COMPLETE => STATUS_COMPLETED,
+            self::ACTION_REFUSE => STATUS_FAILED,
+            self::ACTION_RESPOND => $this->current_status
+        ];
+
+        if (array_key_exists($action, $links)) {
+            return $links[$action];
+        }
+
+        return null;
+    }
+}
