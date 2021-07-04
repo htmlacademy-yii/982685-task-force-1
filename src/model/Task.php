9b248fd4923a00b72cb26eca4b2764e87c694914
Somov Eugene <somov.eugene.2012@gmail.com>
2021-07-04 16:03:38 +0300

    [ module2-task2] Изменен класс задания для использования с объектами класса действия

--- src/model/Task.php
+++ src/model/Task.php
@@ -1,16 +1,64 @@
 <?php
 namespace taskforce\model;
 
+use taskforce\actions\{AppointAction, CancelAction, ChatAction, CompleteAction, RefuseAction, RespondAction};
+
 class Task
 {
     /**
-     * список всех доступных статусов задач
+     * список всех состояний (статусов) заданий и действий
      */
-    const STATUS_NEW = 'new';               // Новое - задание опубликовано, исполнитель ещё не найден
-    const STATUS_CANCELLED = 'cancelled';   // Отменено - заказчик отменил задание
-    const STATUS_PROGRESS = 'progress';     // В работе - заказчик выбрал исполнителя для задания
+    const STATUS_NEW = 'new';               // Новое     - задание опубликовано, исполнитель ещё не найден
+    const STATUS_CANCELLED = 'cancelled';   // Отменено  - заказчик отменил задание
+    const STATUS_PROGRESS = 'progress';     // В работе  - заказчик выбрал исполнителя для задания
     const STATUS_COMPLETED = 'completed';   // Выполнено - заказчик отметил задание как выполненное
     const STATUS_FAILED = 'failed';         // Провалено - исполнитель отказался от выполнения задания
+
+    const ACTION_CANCEL = CancelAction::class;
+    const ACTION_RESPOND = RespondAction::class;
+    const ACTION_APPOINT = AppointAction::class;
+    const ACTION_COMPLETE = CompleteAction::class;
+    const ACTION_REFUSE = RefuseAction::class;
+    const ACTION_CHAT = ChatAction::class;
+
+    /**
+     * доступные действия и изменения состояния задания
+     */ 
+    const STATUSES_MAP = [
+        self::STATUS_NEW => 'новое',
+        self::STATUS_CANCELLED => 'отменено',
+        self::STATUS_PROGRESS => 'в работе',
+        self::STATUS_COMPLETED => 'выполнено',
+        self::STATUS_FAILED => 'провалено',
+    ];
+    
+    const ACTIONS_MAP = [
+        self::ACTION_CANCEL => 'отменить',
+        self::ACTION_APPOINT => 'назначить',
+        self::ACTION_COMPLETE => 'выполнено',
+        self::ACTION_REFUSE => 'отказаться',
+        self::ACTION_CHAT  => 'отправить',
+    ];
+
+    const NEXT_STATUSES_MAP = [
+        self::ACTION_CANCEL => self::STATUS_CANCELLED,
+        self::ACTION_APPOINT => self::STATUS_PROGRESS,
+        self::ACTION_COMPLETE => self::STATUS_COMPLETED,
+        self::ACTION_REFUSE => self::STATUS_FAILED,
+    ];
+
+    const AVAILABLE_ACTIONS_MAP = [
+        self::STATUS_NEW => [               // НОВОЕ ЗАДАНИЕ:
+            self::ACTION_CANCEL,            // Заказчик может отменить задание
+            self::ACTION_RESPOND,           // Исполнитель может откликнуться на задание
+            self::ACTION_APPOINT,           // Заказчик может выбрать исполнителя для задания
+        ],
+        self::STATUS_PROGRESS => [          // ЗАДАНИЕ В РАБОТЕ:
+            self::ACTION_COMPLETE,          // Заказчик может отметить задание как выполненное
+            self::ACTION_REFUSE,            // Исполнитель может отказаться от выполнения задания
+            self::ACTION_CHAT,              // Заказчик и исполнитель могут отправлять сообщения друг другу
+        ],
+    ];
 
     /**
      * список всех возможных ролей пользователей
@@ -19,77 +67,80 @@ class Task
     const ROLE_EXECUTOR = 'executor';       // Исполнитель
 
     /**
-     * список всех доступных действий над задачами
-     */
-    // Заказчик
-    const ACTION_CANCEL = 'cancel';         // Отменить
-    const ACTION_APPOINT = 'appoint';       // Назначить
-    const ACTION_COMPLETE = 'complete';     // Завершить
-    // Исполнитель
-    const ACTION_RESPOND = 'respond';       // Откликнуться
-    const ACTION_REFUSE = 'refuse';         // Отказаться
-
-    /**
      * СВОЙСТВА класса
      */
-    private $id_customer = null;            // id заказчика
-    private $id_executor = null;            // id исполнителя
-    private $completion_date = null;        // срок завершения задачи
-    private $current_status = null;         // активный статус
+    private $customerId;                    // ID заказчика
+    private $executorId;                    // ID исполнителя
+    private $completionDate;                // Срок завершения задания
+    private $status;                        // Текущий статус задания
 
     /**
      * КОНСТРУКТОР класса
-     * @param int $id_customer ID заказчика
-     * @param int $id_executor ID исполнителя
-     * @param string $completion_date Срок завершения задачи
-     * @param string $status Статус задачи
+     * @param int $customerId               ID заказчика (существует всегда)
+     * @param string $completionDate        Срок завершения задания (может отсутствовать)
      */
-    public function __construct(int $id_customer, ?int $id_executor = null, ?string $completion_date = null, ?string $status = self::STATUS_NEW)
+    public function __construct(int $customerId, ?string $completionDate = null)
     {
-        $this->id_customer = $id_customer;
-        $this->id_executor = $id_executor;
-        $this->completion_date = $completion_date;
-        $this->current_status = $status;
+        $this->customerId = $customerId;
+        $this->executorId = null;           // для нового задания исполнитель отсутствует
+        $this->completionDate = $completionDate;
+        $this->status = self::STATUS_NEW;
     }
 
     // МЕТОДЫ класса
 
-    /**
-     * Возвращает список действий
-     * @return array
+    /** 
+     * Устанавливает исполнителя для задания
+     * @param int $executorId               ID исполнителя
      */
-    public function getActions(): array
+    public function setExecutor(int $executorId): void
     {
-        return [self::ACTION_CANCEL, self::ACTION_APPOINT, self::ACTION_COMPLETE, self::ACTION_RESPOND, self::ACTION_REFUSE];
+        $this->executorId = $executorId;
     }
 
-    /** Возвращает список статусов
+    /** 
+     * Возвращает список всех состояний задания
      * @return array
      */
     public function getStatuses(): array
     {
-        return [self::STATUS_NEW, self::STATUS_CANCELLED, self::STATUS_PROGRESS, self::STATUS_COMPLETED, self::STATUS_FAILED];
+        return self::STATUSES_MAP;
+    }
+    
+    /**
+     * Возвращает список всех действий, которые можно выполнить над заданием
+     * @return array
+     */
+    public function getActions(): array
+    {
+        return self::ACTIONS_MAP;
+    }
+    
+    /**
+     * Возвращает статус, в который перейдёт задача после указанного действия
+     * @param string $action                Действие
+     * @return string|null
+     */
+    public function getNextStatus(string $action): ?string
+    {
+        return self::NEXT_STATUSES_MAP[$action] ?? null;
     }
 
-    /** Возвращает статус, в который перейдет задача для указанного действия
-     * @param string $action Действие
-     * @return string|null Статус задачи
+    /**
+     * Возвращает список доступных действий для указанного пользователя и статуса задания
+     * @param int $userId                   ID пользователя
+     * @param string $status                Статус задания
+     * @return array|null                   Список доступных действий или null, если действия отсутствуют
      */
-    public function getNextStatus(string $action): string
+    public function getAvailableActions(int $userId, string $status): ?array
     {
-        // доступные действия и изменение состояния задачи
-        $links = [
-            self::ACTION_CANCEL => STATUS_CANCELLED,
-            self::ACTION_APPOINT => STATUS_PROGRESS,
-            self::ACTION_COMPLETE => STATUS_COMPLETED,
-            self::ACTION_REFUSE => STATUS_FAILED,
-            self::ACTION_RESPOND => null
-        ];
-
-        if (array_key_exists($action, $links)) {
-            return $links[$action];
+        if (!isset(self::AVAILABLE_ACTIONS_MAP[$status])) {
+            return null;
         }
 
-        return null;
+        return array_filter(self::AVAILABLE_ACTIONS_MAP[$status],
+            function ($action) use ($userId) {
+                return $action::isAllowed($userId, $this->customerId, $this->executorId);
+            });
     }
 }
