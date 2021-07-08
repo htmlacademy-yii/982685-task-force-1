f107e5ec87893364173a46231f6889c834f8010c
Somov Eugene <somov.eugene.2012@gmail.com>
2021-07-08 18:44:05 +0300

    [module2-task2] исправлены проверки

--- index.php
+++ index.php
@@ -25,37 +25,41 @@ require_once 'vendor/autoload.php';
 $customerId = 1;
 $executorId = 2;
 
+$actionCancel = new CancelAction;
+$actionAppoint = new AppointAction;
+$actionComplete = new CompleteAction;
+$actionRefuse = new RefuseAction;
+$actionRespond = new RespondAction;
+$actionChat = new ChatAction;
+
 $task = new Task($customerId);
 
-assert($task->getStatuses() === Task::STATUSES_MAP, 'Список статусов заданий');
-assert($task->getActions() === Task::ACTIONS_MAP, 'Список действий');
-
-assert($task->getNextStatus(Task::ACTION_CANCEL) === Task::STATUS_CANCELLED, 'Отменить --> cancelled');
-assert($task->getNextStatus(Task::ACTION_APPOINT) === Task::STATUS_PROGRESS, 'Назначить --> progress');
-assert($task->getNextStatus(Task::ACTION_COMPLETE) === Task::STATUS_COMPLETED, 'Выполнено --> completed');
-assert($task->getNextStatus(Task::ACTION_REFUSE) === Task::STATUS_FAILED, 'Отказаться --> failed');
-assert(is_null($task->getNextStatus(Task::ACTION_RESPOND)), 'Откликнуться --> нет изменения статуса задания');
-assert(is_null($task->getNextStatus(Task::ACTION_CHAT)), 'Отправить (сообщение) --> нет изменения статуса задания');
+assert($task->getNextStatus($actionCancel) === Task::STATUS_CANCELLED, 'Отменить --> cancelled');
+assert($task->getNextStatus($actionAppoint) === Task::STATUS_PROGRESS, 'Назначить --> progress');
+assert($task->getNextStatus($actionComplete) === Task::STATUS_COMPLETED, 'Выполнено --> completed');
+assert($task->getNextStatus($actionRefuse) === Task::STATUS_FAILED, 'Отказаться --> failed');
+assert(is_null($task->getNextStatus($actionRespond)), 'Откликнуться --> нет изменения статуса задания');
+assert(is_null($task->getNextStatus($actionChat)), 'Отправить (сообщение) --> нет изменения статуса задания');
 
 // Доступные действия для заказчика
 $userId = $customerId;
 
-assert($task->getAvailableActions($userId, Task::STATUS_NEW) === [0 => CancelAction::class], 'STATUS_NEW: заказчик может отменить задание');
-assert(in_array([0 => CompleteAction::class], $task->getAvailableActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: заказчик может отметить, что задание выполнено');
-assert(in_array([2 => ChatAction::class], $task->getAvailableActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: заказчик может отправить сообщение исполнителю');
-assert(is_null($task->getAvailableActions($userId, Task::STATUS_CANCELLED)), 'STATUS_CANCELLED: нет действия ');
-assert(is_null($task->getAvailableActions($userId, Task::STATUS_COMPLETED)), 'STATUS_COMPLETED: нет действия');
-assert(is_null($task->getAvailableActions($userId, Task::STATUS_FAILED)), 'STATUS_FAILED: нет действия');
+assert($task->getActions($userId, Task::STATUS_NEW) === [0 => CancelAction::class], 'STATUS_NEW: заказчик может отменить задание');
+assert(in_array([0 => CompleteAction::class], $task->getActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: заказчик может отметить, что задание выполнено');
+assert(in_array([2 => ChatAction::class], $task->getActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: заказчик может отправить сообщение исполнителю');
+assert(is_null($task->getActions($userId, Task::STATUS_CANCELLED)), 'STATUS_CANCELLED: нет действия ');
+assert(is_null($task->getActions($userId, Task::STATUS_COMPLETED)), 'STATUS_COMPLETED: нет действия');
+assert(is_null($task->getActions($userId, Task::STATUS_FAILED)), 'STATUS_FAILED: нет действия');
 
 // Доступные действия для исполнителя
 $task->setExecutor($executorId);
 $userId = $executorId;
 
-assert($task->getAvailableActions($userId, Task::STATUS_NEW) === [1 => RespondAction::class], 'STATUS_NEW: исполнитель может откликнуться на задание');
-assert(in_array([1 => RefuseAction::class], $task->getAvailableActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: исполнитель может отказаться от задания');
-assert(in_array([2 => ChatAction::class], $task->getAvailableActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: исполнитель может отправить сообщение заказчику');
-assert(is_null($task->getAvailableActions($userId, Task::STATUS_CANCELLED)), 'STATUS_CANCELLED: нет действия');
-assert(is_null($task->getAvailableActions($userId, Task::STATUS_COMPLETED)), 'STATUS_COMPLETED: нет действия');
-assert(is_null($task->getAvailableActions($userId, Task::STATUS_FAILED)), 'STATUS_FAILED: нет действия');
+assert($task->getActions($userId, Task::STATUS_NEW) === [1 => RespondAction::class], 'STATUS_NEW: исполнитель может откликнуться на задание');
+assert(in_array([1 => RefuseAction::class], $task->getActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: исполнитель может отказаться от задания');
+assert(in_array([2 => ChatAction::class], $task->getActions($userId, Task::STATUS_PROGRESS)), 'STATUS_PROGRESS: исполнитель может отправить сообщение заказчику');
+assert(is_null($task->getActions($userId, Task::STATUS_CANCELLED)), 'STATUS_CANCELLED: нет действия');
+assert(is_null($task->getActions($userId, Task::STATUS_COMPLETED)), 'STATUS_COMPLETED: нет действия');
+assert(is_null($task->getActions($userId, Task::STATUS_FAILED)), 'STATUS_FAILED: нет действия');
 
 echo 'Все проверки пройдены';
