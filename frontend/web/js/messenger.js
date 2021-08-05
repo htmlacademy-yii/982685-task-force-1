9871d2399a50b9c99554a70ee90dfeb80b297149
Somov Eugene <somov.eugene.2012@gmail.com>
2021-08-05 21:55:24 +0300

    Revert "в вёрстку проекта внесены глобальные обновления (см. группу в Slack, #news от 04 февраля 2021 г.)"
    This reverts commit 9c58c4de61de7d6c702f570a409fe9db22adf248.

--- frontend/web/js/messenger.js
+++ frontend/web/js/messenger.js
@@ -20,7 +20,7 @@ Vue.component('chat', {
       console.error("Не передан идентификатор задания (атрибут task) в теге 'chat'")
     }
     else {
-      this.api_url = '/index.php/api/messages';
+      this.api_url = '/index.php/api/messages?id=' + this.task;
       this.getMessages();
     }
   },
@@ -28,7 +28,7 @@ Vue.component('chat', {
     sendMessage: function() {
       fetch(this.api_url, {
         method: 'POST',
-        body: JSON.stringify({message: this.message, task_id: this.task})
+        body: JSON.stringify({message: this.message})
       })
       .then(result => {
         if (result.status !== 201) {
@@ -46,7 +46,7 @@ Vue.component('chat', {
       })
     },
     getMessages: function () {
-      fetch(this.api_url + '?task_id=' + this.task)
+      fetch(this.api_url)
       .then(result => {
         if (result.status !== 200) {
           return Promise.reject(new Error('Запрошенный ресурс не существует'));
